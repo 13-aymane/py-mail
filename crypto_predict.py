01e8a53dc6e8104a334b5b01155499c8aa3c8bcf
aymane <13.aymane@gmail.com>
2022-09-01 12:54:02 +0100

    c

--- crypto_predict.py
+++ crypto_predict.py
@@ -23,12 +23,13 @@ scaler = MinMaxScaler(feature_range=(0,1))
 scaled_data = scaler.fit_transform(data['Close'].values.reshape(-1,1))
 
 prediction_days = 60
+future_day = 30
 
 x_train, y_train = [], []
 
-for x in range (prediction_days, len(scaled_data)):
+for x in range (prediction_days, len(scaled_data)-future_day):
     x_train.append(scaled_data[x-prediction_days:x, 0])
-    y_train.append(scaled_data[x, 0])
+    y_train.append(scaled_data[x+future_day, 0])
 
 x_train, y_train = np.array(x_train), np.array(y_train)
 x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
@@ -44,7 +45,7 @@ model.add(Dropout(0.2))
 model.add(Dense(units=1))
 
 model.compile(optimizer='adam', loss ='mean_squared_error')
-model.fit(x_train,y_train, epochs=25, batch_size=32)
+model.fit(x_train,y_train, epochs=20, batch_size=32)
 
 #testing
 
